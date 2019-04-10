274f65a9c41939ab8ac230f6c1851b5b2bf6fe1f
Pablo <pablopst@gmail.com>
2019-04-09 23:02:44 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import SquareCollab from "./components/SquareCollab";
+
+const App = () => <SquareCollab />;
+
+
+export default App;
+
---
+++ src/components/SquareCollab/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const SquareCollab = () => (
+    <button className="square-collab">X</button>
+);
+
+export default SquareCollab;
